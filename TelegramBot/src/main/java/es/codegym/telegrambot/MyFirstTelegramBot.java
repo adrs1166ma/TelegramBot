cad0324b180d726eafcf4a12005dd22d1c42996a
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-04-10 13:27:26 -0500

    base

---
+++ TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,66 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "ChatBotadrs1166ma_bot";
+    public static final String TOKEN = "7009056979:AAFqQpKfDPgYCCXjiJSjrmOepVv-zRa_ctY";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+
+        if (getMessageText().equals("/start")){
+            setUserGlory(0);
+            sendTextMessageAsync(STEP_1_TEXT,
+                    Map.of("Hackear la nevera","step_1_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_1_btn")){
+            setUserGlory(20);
+            sendTextMessageAsync(STEP_2_TEXT,
+                    Map.of("¡Tomar una salchicha! +20 de fama","step_2_btn",
+                            "¡Tomar un pescado! +20 de fama","step_2_btn",
+                            "¡Tirar una lata de pepinillos! +20","step_2_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_2_btn")){
+            setUserGlory(20);
+            sendTextMessageAsync(STEP_3_TEXT,
+                    Map.of("Hackear al robot aspiradora","step_3_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_3_btn")){
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of("Enviar al robot aspirador por comida! +30 de fama","step_4_btn",
+                            "Dar un paseo en el robot aspirador! +30 de fama","step_4_btn",
+                            "¡Huir del robot aspirador! +30 de fama","step_4_btn"));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_4_btn")){
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_5_TEXT,
+                    Map.of());
+        }
+
+
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
